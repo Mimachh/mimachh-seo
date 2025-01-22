211f8f999466ab681bca2cb7118f45c5f43ee0b9
Karl <106066885+Mimachh@users.noreply.github.com>
2025-01-22 13:36:29 +0100

    Update SeoForm.tsx
    Add calculate caracters

--- src/resources/js/Components/SeoForm.tsx
+++ src/resources/js/Components/SeoForm.tsx
@@ -1,50 +1,88 @@
-import FormFieldLayout from "@/Components/mimachh/2fa/Form/form-field-layout";
 import { Input } from "@/Components/ui/input";
 import { Textarea } from "@/Components/ui/textarea";
+import FormFieldLayout from "@/Pages/Auth/Partials/Form/form-field-layout";
+import { useState } from "react";
 
 interface Props {
     data: {
-        title_seo: string;
-        description_seo: string;
+        seo_title?: string;
+        seo_description?: string;
     };
     setData: (key: string, value: string) => void;
-    errors: Partial<Record<"description_seo" | "title_seo", string>>;
+    errors: Partial<Record<"seo_description" | "seo_title", string>>;
 }
 const SeoForm = (props: Props) => {
     const { data, setData, errors } = props;
+
+    const [charCount, setCharCount] = useState({
+        seo_title: data.seo_title?.length || 0,
+        seo_description: data.seo_description?.length || 0,
+    });
+    const handleInputChange = (
+        key: "seo_title" | "seo_description",
+        value: string,
+    ) => {
+        setData(key, value);
+        setCharCount((prev) => ({ ...prev, [key]: value.length }));
+    };
+    const isValidRange = (length: number, min: number, max: number) => {
+        return length >= min && length <= max;
+    };
     return (
         <div className="space-y-3">
             <FormFieldLayout
-                fieldName="title_seo"
+                fieldName="seo_title"
                 label="Titre SEO"
-                error={errors.title_seo}
+                error={errors.seo_title}
             >
                 <Input
-                    id="title_seo"
+                    id="seo_title"
                     type="text"
-                    name="title_seo"
+                    name="seo_title"
                     placeholder="Votre titre SEO"
-                    value={data.title_seo}
+                    value={data.seo_title}
                     className="mt-1 block w-full py-3 border"
-                    onChange={(e) => setData("title_seo", e.target.value)}
+                    onChange={(e) =>
+                        handleInputChange("seo_title", e.target.value)
+                    }
                 />
-                 <small>Entre 50 et 60 caractères maximum</small>
+                <small
+                    className={`${
+                        isValidRange(charCount.seo_title, 50, 60)
+                            ? "text-green-500"
+                            : "text-orange-500"
+                    }`}
+                >
+                    {charCount.seo_title} caractères (Idéalement entre 50 et 60
+                    caractères maximum)
+                </small>
             </FormFieldLayout>
 
             <FormFieldLayout
-                fieldName="description_seo"
+                fieldName="seo_description"
                 label="Description SEO"
-                error={errors.description_seo}
+                error={errors.seo_description}
             >
                 <Textarea
                     placeholder="Votre description SEO"
                     className="resize-none"
-                    id="description_seo"
-                    name="description_seo"
-                    value={data.description_seo}
-                    onChange={(e) => setData("description_seo", e.target.value)}
+                    id="seo_description"
+                    name="seo_description"
+                    value={data.seo_description}
+                    onChange={(e) =>
+                        handleInputChange("seo_description", e.target.value)
+                    }
                 />
-                <small>Entre 50 et 160 caractères maximum</small>
+                <small
+                    className={`${
+                        isValidRange(charCount.seo_description, 50, 160)
+                            ? "text-green-500"
+                            : "text-orange-500"
+                    }`}
+                >
+                    {charCount.seo_description} caractères (Idéalement entre 50
+                    et 160 caractères maximum)
+                </small>
             </FormFieldLayout>
         </div>
     );
