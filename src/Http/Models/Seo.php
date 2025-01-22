89696ab0caa0dec445e676dd60ed44345707b74a
Karl <106066885+Mimachh@users.noreply.github.com>
2025-01-22 13:53:04 +0100

    Update Seo.php

--- src/Http/Models/Seo.php
+++ src/Http/Models/Seo.php
@@ -8,7 +8,7 @@ class Seo extends Model
 {
     //table name
     protected $table = 'seo';
-
+    protected $seo_data = null; // Propriété temporaire pour stocker les données SEO en mémoire
     protected $fillable = ['seo_title', 'seo_description'];
 
     public function seoable()
